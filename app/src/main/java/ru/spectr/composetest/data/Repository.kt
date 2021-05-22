c90a635d14275bda4c1c161788c4d2bcc1624b49
Sergey <Serge-shilovski@yandex.ru>
2021-05-22 03:03:30 +0300

    refactor

--- app/src/main/java/ru/spectr/composetest/data/Repository.kt
+++ app/src/main/java/ru/spectr/composetest/data/Repository.kt
@@ -1,7 +1,15 @@
 package ru.spectr.composetest.data
 
-class Repository {
-    suspend fun getSessions() = MockSessions
+import kotlinx.coroutines.delay
 
-    suspend fun getSession(id: String) = MockSessions.find { it.id == id }
+class Repository {
+    suspend fun getSessions(): List<Session> {
+        delay(1000)
+        return MockSessions
+    }
+
+    suspend fun getSession(id: String): Session? {
+        delay(500)
+        return MockSessions.find { it.id == id }
+    }
 }
